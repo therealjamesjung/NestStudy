c306774fc6cb2f9fb6c8ee4207507589edcf2f38
James Jung <therealjamesjung@gmail.com>
2023-06-21 17:48:13 +0900

    split app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from 'src/post/post.module';
 
 @Module({
-  imports: [],
+  imports: [PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
